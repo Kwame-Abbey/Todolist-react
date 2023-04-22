dbaec1151d3aa5c5b4424237568c7dad00884a0a
unknown <constancekwameabbey@gmail.com>
2023-04-22 08:53:35 +0100

    Create TodoItem and InputArea components

---
+++ src/components/InputArea.js
@@ -0,0 +1,12 @@
+export default function InputArea() {
+
+    return (
+        <div className="form">
+        <input  type="text"  />
+        <button >
+          <span>Add</span>
+        </button>
+      </div>
+    );
+    
+}
---
+++ src/components/TodoItem.js
@@ -0,0 +1,8 @@
+export default function TodoItem() {
+
+    return (
+        <div>
+            <li></li>
+        </div>
+    )
+}
